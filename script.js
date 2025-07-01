b89a808fa0197dfe6904833a387045dbd1d98afb
pppatil7 <pundalikpatil7218@gmail.com>
2025-07-01 20:40:51 +0530

    uncomment

--- script.js
+++ script.js
@@ -1,73 +1,73 @@
 // console.log("scipt loaded");
 // console.log("this is testing");
 
-// //ES6+ Syntax
-// // function fun1() {
-// //     console.log("this is fun1");
-// // }
-
-// // fun1();
-
-// const fun2 = () => {
-//     console.log("this is arrow function");
-// };
-
-// fun2();
-
-// function funTest(fun) {
-//     fun();
-//     return fun;
+//ES6+ Syntax
+// function fun1() {
+//     console.log("this is fun1");
 // }
-// //Arrow Functions
-// funTest(() => {
-//     console.log("This is fun body");
-// });
 
-// const sum = (a, b) => a + b;
+// fun1();
 
-// console.log(sum(1, 2));
+const fun2 = () => {
+    console.log("this is arrow function");
+};
 
-// //Destructuring
+fun2();
 
-// const users = ["john", "peter", "kane", "david"];
-// const [name1, name2, name3, name4] = users;
-// console.log(name1);
-// console.log(name2);
-// console.log(name3);
-// console.log(name4);
+function funTest(fun) {
+    fun();
+    return fun;
+}
+//Arrow Functions
+funTest(() => {
+    console.log("This is fun body");
+});
 
-// const product = {
-//     productId: 45,
-//     productName: "Iphone",
-//     price: 82736,
-//     discount: 10
-// };
+const sum = (a, b) => a + b;
 
-// //console.log(product);
-// const { productId, productName, price, discount } = product;
-// console.log(productId);
-// console.log(productName);
-// console.log(price);
-// console.log(discount);
+console.log(sum(1, 2));
 
-// //Template Literals
-// let price1 = 292;
-// let discount1 = 10;
-// let para = `The price of the product is ${price1} and discount is ${discount1}`;
-// console.log(para);
+//Destructuring
 
-// //Spread and Rest Operators
+const users = ["john", "peter", "kane", "david"];
+const [name1, name2, name3, name4] = users;
+console.log(name1);
+console.log(name2);
+console.log(name3);
+console.log(name4);
 
-// const activeUsers = [...users, "manish", "Raj", ...users];
-// console.log(activeUsers);
+const product = {
+    productId: 45,
+    productName: "Iphone",
+    price: 82736,
+    discount: 10
+};
 
-// const newProduct = {
-//     ...product,
-//     live: false,
-//     category: "trending"
-// };
+//console.log(product);
+const { productId, productName, price, discount } = product;
+console.log(productId);
+console.log(productName);
+console.log(price);
+console.log(discount);
 
-// console.log(newProduct);
+//Template Literals
+let price1 = 292;
+let discount1 = 10;
+let para = `The price of the product is ${price1} and discount is ${discount1}`;
+console.log(para);
+
+//Spread and Rest Operators
+
+const activeUsers = [...users, "manish", "Raj", ...users];
+console.log(activeUsers);
+
+const newProduct = {
+    ...product,
+    live: false,
+    category: "trending"
+};
+
+console.log(newProduct);
 
 
 //closures
